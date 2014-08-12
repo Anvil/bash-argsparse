e812614e99d651572cfebf2396134c1faa490b3b
Damien Nadé <github@livna.org>
2014-08-12 10:13:14 +0200

    1.6 version

--- argsparse.sh
+++ argsparse.sh
@@ -5,7 +5,7 @@
 ## @author Damien Nadé <bash-argsparse@livna.org>
 ## @brief Bash Argsparse Library
 ## @copyright WTFPLv2
-## @version 1.5
+## @version 1.6
 #
 #########
 # License:
@@ -267,7 +267,7 @@ fi
 ## @var ARGSPARSE_VERSION
 ## @brief argsparse version number
 ## @showinitializer
-declare -r ARGSPARSE_VERSION=1.5
+declare -r ARGSPARSE_VERSION=1.6
 
 # Enable required features
 shopt -s extglob
